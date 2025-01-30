e9e72f474c5ced3da80e2af70283914794b4ce05
Davison Mota <davimotagnr@gmail.com>
2025-01-30 15:24:34 -0300

    chore: config setup Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('RealWorld App Conduit')
+    .setDescription('Conduit is a social blogging site')
+    .setVersion('1.0')
+    .build()
+
+  const docmentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, docmentFactory)
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
